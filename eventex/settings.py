e308e6bd724c9f60bfba5fa8678b5ccb8bd3acd0
Victor Westmann <victor.westmann@gmail.com>
2014-02-10 23:55:27 -0200

    Debug false

--- eventex/settings.py
+++ eventex/settings.py
@@ -8,14 +8,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
-'''
-DESATIVADO POR VICTOR WESTMANN
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-DESATIVADO POR VICTOR WESTMANN
-'''
-import dj_database_url
+from decouple import config
+from dj_database_url import parse as db_url
 from unipath import Path
 BASE_DIR = Path(__file__).parent
 
@@ -27,11 +21,11 @@ BASE_DIR = Path(__file__).parent
 SECRET_KEY = 'l8_go7$i@u+xnt9o)=fh_#3ty!$$(dc4_usu2p&_=(95njzc0i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = DEBUG
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.localhost', '127.0.0.1', 'herokuapps.com']
 
 
 # Application definition
@@ -64,8 +58,10 @@ WSGI_APPLICATION = 'eventex.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-    	default='sqlite:///' + BASE_DIR.child('db.sqlite3'))
+    'default': config(
+        'DATABASE_URL',
+    	default='sqlite:///' + BASE_DIR.child('db.sqlite3'),
+        cast=db_url),
 }
 
 # Internationalization
